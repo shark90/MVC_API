279b840702ad2e01a5bb2f064ae5497b89f03fb6
Deepak <deepakmishra90@hotmail.com>
2017-08-24 16:56:36 +0530

    Added

---
+++ YetAnotherTodo.WebApi/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Owin;
+using YetAnotherTodo.EF;
+using YetAnotherTodo.EF.Repository;
+using YetAnotherTodo.WebApi.Models;
+
+[assembly: OwinStartup(typeof(YetAnotherTodo.WebApi.Startup))]
+
+namespace YetAnotherTodo.WebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            app.CreatePerOwinContext<TodoItemRepository>((fo, ctx) => new TodoItemRepository(ctx.Get<YetAnotherTodoDbContext<ApplicationUser>>()));
+        }
+    }
+}

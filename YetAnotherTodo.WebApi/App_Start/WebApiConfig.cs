fc00b0e7b0c0d353a9638bf4551105b77abc0172
Deepak <deepakmishra90@hotmail.com>
2017-08-24 16:45:08 +0530

    Added

---
+++ YetAnotherTodo.WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.OAuth;
+
+namespace YetAnotherTodo.WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            config.Filters.Add(new HostAuthenticationFilter(DefaultAuthenticationTypes.ApplicationCookie));
+
+            //Enable CORS
+            config.EnableCors();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
